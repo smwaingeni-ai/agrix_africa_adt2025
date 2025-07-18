fd89c1120ce10dca5149cd36df62990a60d2ecc2
smwaingeni-ai <smwaingeni.ai@gmail.com>
2025-07-18 15:50:41 +0300

    Update farmer_profile_screen.dart

--- lib/screens/profile/farmer_profile_screen.dart
+++ lib/screens/profile/farmer_profile_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
-import '../../models/farmer_profile.dart';
-import '../../services/profile_service.dart';
+import 'package:agrix_africa_adt2025/models/farmer_profile.dart';
+import 'package:agrix_africa_adt2025/services/farmer_service.dart';
 
 class FarmerProfileScreen extends StatelessWidget {
   const FarmerProfileScreen({super.key});
